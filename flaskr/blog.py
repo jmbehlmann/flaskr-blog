d7d4bc7b5703215c492edcee2314e727afc7e74c
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2024-04-07 14:35:03 -0500

    show action

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -94,3 +94,8 @@ def delete(id):
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
+
+@bp.route('/<int:id>', methods=('GET',))
+def show(id):
+    post = get_post(id)
+    return render_template('blog/show.html', post=post)
